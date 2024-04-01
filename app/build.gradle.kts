c2428af45c5d7da7fefde63e420d2a4fd866a4e1
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2024-04-01 15:51:24 +0100

    UPDATE VERSION

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "github.heinrichbarth.meccgevents"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 20250101
+        versionName = "v20250101"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
