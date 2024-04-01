ac5aa76803642f7038a15c1abec9f385c38c66dd
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2024-04-01 14:18:30 +0100

    organize import

--- app/src/main/java/github/heinrichbarth/meccgevents/ui/news/NewsDetailsFragment.java
+++ app/src/main/java/github/heinrichbarth/meccgevents/ui/news/NewsDetailsFragment.java
@@ -5,10 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
